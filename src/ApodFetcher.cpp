e0cba562aa6ec16f130eac66ae6981d092645fb5
clovis-p <clovis-p@proton.me>
2026-01-04 19:29:54 -0500

    Workaround for GitHub caching issue

--- src/ApodFetcher.cpp
+++ src/ApodFetcher.cpp
@@ -9,6 +9,7 @@
 #include "FsUtils.h"
 
 ApodFetcher::ApodFetcher(const std::string &url) : httpClient(url) {
+    // todo: what is that doing here
     std::cout << "Starting HTTP client with URL " + url << std::endl;
 }
 
